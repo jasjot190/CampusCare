e4d5ff996c08fd33519cfbdfdc4cf088e748a9d9
jasjot190 <jasjot.singh@edu.sait.ca>
2024-11-01 16:56:01 -0600

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function index() {
+  return (
+    <View
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This is the Landing page of CampusCare App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
